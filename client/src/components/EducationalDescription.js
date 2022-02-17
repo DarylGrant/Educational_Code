32f1df103eab349322a5b1b582f1127297255116
Dudge1991 <dudge1@hotmail.co.uk>
2022-02-17 15:23:24 +0000

    video link showing

--- client/src/components/EducationalDescription.js
+++ client/src/components/EducationalDescription.js
@@ -1,23 +1,21 @@
 import React from "react";
 import LinkList from "./LinkList";
 import ImageList from "./ImageList";
+import VideoList from "./VideoList";
 
 const EducationalDescription = ({ topic }) => {
-
-  
-
-
-    return (
-        <>
-          <h2>{topic.title}</h2>
-          <h3>{topic.name}</h3>
-          <p>{topic.description}</p>
-          {topic.links && <LinkList links={topic.links} />}
-          <h4>Examples</h4>
-          <p>{topic.examples}</p>
-          {topic.image && <ImageList images={topic.image}/>}
-        </>
-    )
-}
+  return (
+    <>
+      <h2>{topic.title}</h2>
+      <h3>{topic.name}</h3>
+      <p>{topic.description}</p>
+      {topic.links && <LinkList links={topic.links} />}
+      <h4>Examples</h4>
+      <p>{topic.examples}</p>
+      {topic.image && <ImageList images={topic.image} />}
+      {topic.video && <VideoList videos={topic.video} />}
+    </>
+  );
+};
 
 export default EducationalDescription;
